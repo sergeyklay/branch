a994dccfe761db6205ca579e1d60a1e396584ae7
Serghei Iakovlev <egrep@protonmail.ch>
2021-04-03 13:46:32 +0300

    Add AdminModel for Post

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -13,4 +13,44 @@
 # You should have received a copy of the GNU General Public License
 # along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'author',
+        'published',
+        'status',
+    )
+
+    list_filter = (
+        'status',
+        'created',
+        'published',
+        'author',
+    )
+
+    search_fields = (
+        'title',
+        'body',
+    )
+
+    prepopulated_fields = {
+        'slug': ('title',),
+    }
+
+    raw_id_fields = (
+        'author',
+    )
+
+    date_hierarchy = 'published'
+
+    ordering = (
+        'status',
+        'published',
+    )
