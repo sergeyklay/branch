c924d119956d6d072738c414fc1f57172c3ed689
Serghei Iakovlev <egrep@protonmail.ch>
2021-04-04 13:40:40 +0300

    Setup static files and templates

--- branch/settings/base.py
+++ branch/settings/base.py
@@ -97,7 +97,7 @@ ROOT_URLCONF = 'branch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
