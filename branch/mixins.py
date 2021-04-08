d5becdc71d3c36eca3fbdc221b3c1cfc272c72b6
Serghei Iakovlev <egrep@protonmail.ch>
2021-04-08 14:46:28 +0300

    Add Dublin Core metadata support

--- branch/mixins.py
+++ branch/mixins.py
@@ -21,6 +21,7 @@ class PageDetailsMixin:
 
     description = None
     title = None
+    author = None
 
     def get_title(self):
         """Get object title."""
@@ -30,15 +31,24 @@ class PageDetailsMixin:
         """Get description title."""
         return self.description
 
+    def get_author(self):
+        """Get object author."""
+        return self.author
+
     def get_context_data(self, **kwargs):
         """Get object's context data to use in templates."""
         context = super().get_context_data(**kwargs)
+
         title = self.get_title()
         if title:
-            context['page_title'] = self.get_title()
+            context['page_title'] = title
 
         description = self.get_description()
         if description:
-            context['page_description'] = self.get_description()
+            context['page_description'] = description
+
+        author = self.get_author()
+        if author:
+            context['page_author'] = author
 
         return context
