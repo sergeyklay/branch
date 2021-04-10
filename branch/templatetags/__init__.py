15c2732efe2271f767b6c553dcee77cf0b8da223
Serghei Iakovlev <egrep@protonmail.ch>
2021-04-11 01:18:50 +0300

    Code cleanup

--- branch/templatetags/__init__.py
+++ branch/templatetags/__init__.py
@@ -12,32 +12,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this file.  If not, see <https://www.gnu.org/licenses/>.
-
-"""The top-level module for branch package.
-
-This module tracks the version of the package as well as the base
-package info used by various functions within Branch.
-
-Misc variables:
-
-    __copyright__
-    __version__
-    __license__
-    __author__
-    __author_email__
-    __url__
-    __description__
-
-Refer to the `documentation <https://github.com/sergeyklay/branch/>`_
-for details on the use of this package.
-
-"""
-
-__copyright__ = 'Copyright (C) 2021 Serghei Iakovlev'
-__version__ = '0.0.1'
-__license__ = 'GPLv3+'
-__author__ = 'Serghei Iakovlev'
-__author_email__ = 'egrep@protonmail.ch'
-__url__ = 'https://github.com/sergeyklay/branch'
-__description__ = ('A Django blog application with features '
-                   'of a standard blogging platform.')
