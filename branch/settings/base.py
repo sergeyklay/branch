26b2f2b6610a828205d838098496744ce0356e96
Serghei Iakovlev <egrep@protonmail.ch>
2021-04-12 01:29:48 +0300

    Enable locale middleware

--- branch/settings/base.py
+++ branch/settings/base.py
@@ -103,6 +103,8 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 ROOT_URLCONF = 'branch.urls'
