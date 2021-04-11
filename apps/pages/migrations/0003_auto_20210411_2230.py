6eb52a3a9081071c14f6585f8d50c3eee6c0a048
Serghei Iakovlev <egrep@protonmail.ch>
2021-04-12 01:37:38 +0300

    Add new migrations

---
+++ apps/pages/migrations/0003_auto_20210411_2230.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2021 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pages', '0002_page_locale'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='page',
+            name='locale',
+            field=models.CharField(
+                choices=[
+                    ('en_US', 'English'),
+                    ('ru_RU', 'Russian'),
+                    ('uk_UA', 'Ukrainian'),
+                ],
+                default='en_US',
+                help_text='Specify the publishing locale used.',
+                max_length=5, verbose_name='Locale',
+            ),
+        ),
+        migrations.AlterField(
+            model_name='page',
+            name='slug',
+            field=models.SlugField(
+                help_text='Leave it blank for automatic populating.',
+                max_length=250,
+                unique_for_date='published_at',
+                verbose_name='Slug',
+            ),
+        ),
+    ]
