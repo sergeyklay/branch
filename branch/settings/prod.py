ef68964b08c9c83f77bf40e661a2be08b2feaeab
Serghei Iakovlev <egrep@protonmail.ch>
2021-04-13 19:21:33 +0300

    Secure production configuration

--- branch/settings/prod.py
+++ branch/settings/prod.py
@@ -21,8 +21,38 @@ from .base import *  # noqa
 # SECURITY WARNING: define the correct hosts in production
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
+# A list of IP addresses, as strings, that:
+# - Allow the debug() context processor to add some variables to the template
+#   context.
+# - Can use the admindocs bookmarklets even if not logged in as a staff user.
+# - Are marked as “internal” in AdminEmailHandler emails.
 INTERNAL_IPS = env.list('INTERNAL_IPS')
 
 COMPRESS_OFFLINE = True
 
 EMAIL_BACKEND = env.str('EMAIL_BACKEND')
+
+# Redirect all non-HTTPS requests to HTTPS (except for those URLs matching a
+# regular expression listed in SECURE_REDIRECT_EXEMPT).
+SECURE_SSL_REDIRECT = True
+
+# Add the 'includeSubDomains' directive to the HTTP Strict Transport Security
+# header.
+SECURE_HSTS_SECONDS = 3600
+
+# Add the 'includeSubDomains' directive to the HTTP Strict Transport Security
+# header.
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# Add the 'preload' directive to the HTTP Strict Transport Security header.
+SECURE_HSTS_PRELOAD = True
+
+# Whether to use a secure cookie for the session cookie. If this is set to
+# True, the cookie will be marked as “secure”, which means browsers may ensure
+# that the cookie is only sent under an HTTPS connection.
+SESSION_COOKIE_SECURE = True
+
+# Whether to use a secure cookie for the CSRF cookie. If this is set to True,
+# the cookie will be marked as “secure”, which means browsers may ensure that
+# the cookie is only sent with an HTTPS connection.
+CSRF_COOKIE_SECURE = True
