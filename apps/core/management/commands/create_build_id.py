92455faeaf405fbaf61cff2d698c7174715cbcd9
Serghei Iakovlev <egrep@protonmail.ch>
2021-05-03 01:19:23 +0300

    Code style fixes

--- apps/core/management/commands/create_build_id.py
+++ apps/core/management/commands/create_build_id.py
@@ -13,20 +13,27 @@
 # You should have received a copy of the GNU General Public License
 # along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
-from django.core.management.base import BaseCommand
+"""Provides tools to prepare project to use for production environment."""
+
 import time
 import uuid
+
 from django.conf import settings
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Generate project's build id"
+    """Generate project's build id."""
 
-    def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
+    help = "Generate project's build id"  # noqa: A003
+
+    def __init__(self, stdout=None, stderr=None, no_color=False,
+                 force_color=False):
         super().__init__(stdout, stderr, no_color, force_color)
         self.build_id = None
 
     def update_build_info(self):
+        """Regenerate project's build ids."""
         time_hash = hex(int(time.time()))
         build_id = uuid.uuid4().hex
 
@@ -35,5 +42,5 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.update_build_info()
         build_id_file = settings.BASE_DIR('build.py')
-        with open(build_id_file, 'w') as f:
-            f.write('BUILD_ID = "%s"\n' % self.build_id)
+        with open(build_id_file, 'w') as file:
+            file.write('BUILD_ID = "%s"\n' % self.build_id)
