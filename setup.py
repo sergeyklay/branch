8b999019da81aac7327811b1958804039a5bfb93
Serghei Iakovlev <egrep@protonmail.ch>
2021-06-03 13:02:24 +0300

    Amend documentation

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
+"""Setup module for Branch."""
+
 import codecs
 import re
 from os import path
