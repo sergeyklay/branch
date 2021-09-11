a900a385aa49bb39d1ba1c6072bc2eb16338fa41
Serghei Iakovlev <egrep@protonmail.ch>
2021-09-11 16:46:49 +0300

    Enable admin view to see history/log events

---
+++ apps/core/admin/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2021 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Main Branch admin module."""
+
+from django.contrib import admin
+from django.utils.translation import gettext_lazy
+
+from .model import LogEntryAdmin
+
+admin.site.site_header = gettext_lazy('Branch administration')
+admin.site.site_title = gettext_lazy('Branch site admin')
